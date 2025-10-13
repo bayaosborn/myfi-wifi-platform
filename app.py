4861dde1c044868453066846caff25713066cd51
Baya Osborn <95222324+bayaosborn@users.noreply.github.com>
2025-10-13 03:50:18 +0000

    Updated port to be used on app.py last lines

--- app.py
+++ app.py
@@ -734,5 +734,13 @@ def reset_db():
         return f"Error resetting database: {e}", 500
 
 # ===== THIS MUST BE LAST =====
+
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+#if __name__ == '__main__':
+   # app.run(host='0.0.0.0', port=5000, debug=True)
