79afb8e5f559d206f79f9e035386a0549da38934
1350 CAPITAL <48522292-fadhililumbasi@users.noreply.replit.com>
2025-12-05 22:28:31 +0000

    Disabled logic folder files to debug Render deployment error

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -20,13 +20,13 @@ def register_blueprints(app):
 
     # Import blueprints
     # from app.routes.auth import auth_bp
-    from app.routes.logic_routes import logic_bp
+    #from app.routes.logic_routes import logic_bp
     from app.routes.addCustomContact import addCustomContact_bp
   #  from app.routes.notes import notes_bp
 
     # Register blueprints
     # app.register_blueprint(auth_bp)
-    app.register_blueprint(logic_bp)
+    #app.register_blueprint(logic_bp)
     app.register_blueprint(addCustomContact_bp)
   #  app.register_blueprint(notes_bp)
 
